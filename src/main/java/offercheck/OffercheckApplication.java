2ae8804e83e7b8b5f7bb3a4bd86aa448b3fc962b
Sai Varda <126225887+saivarda@users.noreply.github.com>
2026-04-22 19:39:27 -0500

    feat: add spring boot project scaffold

---
+++ src/main/java/offercheck/OffercheckApplication.java
@@ -0,0 +1,13 @@
+package offercheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffercheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffercheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/offercheck/OffercheckApplicationTests.java
@@ -0,0 +1,13 @@
+package offercheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffercheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
